54d3b0c8ef3dd8988677c271dbf361d31bfc71c5
Jay Carlson <jcarlson@unl.edu>
2018-01-15 17:01:46 -0600

    Hardware reset of PHY at startup

--- firmware/Src/main.c
+++ firmware/Src/main.c
@@ -152,7 +152,21 @@ int main(void)
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 0);
+
+GPIO_InitTypeDef GPIO_InitStruct;
+
+  GPIO_InitStruct.Pin = GPIO_PIN_6;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_Delay(1);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 1);
+  HAL_Delay(1);
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
